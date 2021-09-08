ab3262088fc2acb24695b08fb4608a888ca73f28
ivandhar <ivandarma98@gmail.com>
2021-09-08 14:30:29 +0700

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('v_edit/{id}', [MahasiswaController::class, 'update'])->name('postupdate');
-Route::post('v_edit/{id}', [MahasiswaController::class, 'destroy'])->name('postdelete');
-Route::get('v_edit/{id}', [MahasiswaController::class, 'edit'])->name('getedit');
-Route::get('v_input', [MahasiswaController::class, 'create'])->name('getinput');
-Route::post('v_input', [MahasiswaController::class, 'store'])->name('input');
+Route::middleware('auth')->group(function () {
+    Route::post('v_edit/{id}', [MahasiswaController::class, 'update'])->name('postupdate');
+    Route::post('v_edit/{id}', [MahasiswaController::class, 'destroy'])->name('postdelete');
+    Route::get('v_edit/{id}', [MahasiswaController::class, 'edit'])->name('getedit');
+    Route::get('v_input', [MahasiswaController::class, 'create'])->name('getinput');
+    Route::post('v_input', [MahasiswaController::class, 'store'])->name('input');
+    Route::get('logout', LogoutController::class)->name('logout');
+});
 
-Route::get('logout', LogoutController::class)->name('logout');
+
 Route::get('/', [HomeController::class, 'homeview']);
-Route::get('register', [RegisterController::class, 'create'])->name('register')->middleware("guest");
-Route::post('register', [RegisterController::class, 'store'])->name('register')->middleware("guest");
 
-Route::get('login', [LoginController::class, 'create'])->name('login')->middleware("guest");
-Route::post('login', [LoginController::class, 'store'])->name('login')->middleware("guest");
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store'])->name('register');
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    Route::post('login', [LoginController::class, 'store'])->name('login');
+});
